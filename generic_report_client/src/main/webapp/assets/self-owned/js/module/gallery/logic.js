01472467fac3f3a76249e201f102991c825eb20a
hejunhua <jhhe_hn@foxmail.com>
2017-09-07 21:35:24 +0800

    description and uptoODA

--- generic_report_client/src/main/webapp/assets/self-owned/js/module/gallery/logic.js
+++ generic_report_client/src/main/webapp/assets/self-owned/js/module/gallery/logic.js
@@ -1,155 +1,194 @@
 function initialize() {
-  $(window).bind('scroll', function() {
-    ScrollPOJO.scrollListener();
-  });
-  // retrieveData(1);
-  ScrollPOJO.listener();
+    $(window).bind('scroll', function () {
+        ScrollPOJO.scrollListener();
+    });
+    // retrieveData(1);
+    ScrollPOJO.listener();
 }
 
-var search_saved_self = function() {
-  ScrollPOJO.reset();
-  retrieveData_saved_self();
+var search_description_self = function () {
+    ScrollPOJO.reset();
+    retrieveData_description_self();
 }
 
-var search_shared_self = function() {
-  ScrollPOJO.reset();
-  retrieveData_shared_self();
-}
-var search_shared_all = function() {
-  ScrollPOJO.reset();
-  retrieveData_shared_all();
+var search_saved_self = function () {
+    ScrollPOJO.reset();
+    retrieveData_saved_self();
 }
 
-var search_template_all = function() {
-  ScrollPOJO.reset();
-  retrieveData_template_all();
+var search_shared_self = function () {
+    ScrollPOJO.reset();
+    retrieveData_shared_self();
+}
+var search_shared_all = function () {
+    ScrollPOJO.reset();
+    retrieveData_shared_all();
+}
+
+var search_template_all = function () {
+    ScrollPOJO.reset();
+    retrieveData_template_all();
 }
 
 
 // *******Server Side Retrieve Data JS Code*******
-var retrieveData_saved_self = function() {
-  var keyword = SearchPOJO.keywords;
-  var likeOrMap = {};
-  if (keyword) {
-    likeOrMap = {
-      "stringalpha": keyword
+var retrieveData_description_self = function () {
+    var keyword = SearchPOJO.keywords;
+    var likeOrMap = {
+        "stringbeta": 'DATASET'
+    };
+    if (keyword) {
+        likeOrMap = {
+            "stringalpha": keyword,
+            "stringbeta": 'DATASET'
+        }
     }
-  }
-  var requestPOJO = {
-    "className": "Share",
-    "orderMap": {
-      "id": false
-    },
-    "pageMaxSize": ScrollPOJO.pageMaxSize,
-    "currentPageNumber": ScrollPOJO.page || 1,
-    "likeORMap": likeOrMap,
-    "eqMap": {
-      "username": UserPOJO.user.userName,
-      "type": "MATRIX_REPORT_DRAFT",
-      "deleted": false
-    },
-    "inMap": {},
-  };
-  var data = {
-    'queryJson': $.toJSON(requestPOJO)
-  };
-
-  console.log(JSON.stringify(data));
-  $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
+    var requestPOJO = {
+        "className": "Share",
+        "orderMap": {
+            "id": false
+        },
+        "pageMaxSize": ScrollPOJO.pageMaxSize,
+        "currentPageNumber": ScrollPOJO.page || 1,
+        "likeORMap": likeOrMap,
+        "eqMap": {
+            "type": "MATRIX_REPORT_DRAFT",
+            "username": UserPOJO.user.userName,
+            "tag": "SAVE",
+            "deleted": false
+        },
+        "inMap": {},
+    };
+    var data = {
+        'queryJson': $.toJSON(requestPOJO)
+    };
+    console.log(JSON.stringify(data));
+    $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
 }
 
-var retrieveData_shared_self = function() {
-  var keyword = SearchPOJO.keywords;
-  var likeOrMap = {};
-  if (keyword) {
-    likeOrMap = {
-      "stringalpha": keyword
+var retrieveData_saved_self = function () {
+    var keyword = SearchPOJO.keywords;
+    var likeOrMap = {};
+    if (keyword) {
+        likeOrMap = {
+            "stringalpha": keyword
+        }
     }
-  }
-  var requestPOJO = {
-    "className": "Share",
-    "orderMap": {
-      "id": false
-    },
-    "pageMaxSize": ScrollPOJO.pageMaxSize,
-    "currentPageNumber": ScrollPOJO.page || 1,
-    "likeORMap": likeOrMap,
-    "eqMap": {
-      "username": UserPOJO.user.userName,
-      "type": "MATRIX_REPORT_DRAFT",
-      "tag":"SHARE",
-      "deleted": false
-    },
-    "inMap": {},
-  };
-  var data = {
-    'queryJson': $.toJSON(requestPOJO)
-  };
-  console.log(JSON.stringify(data));
-  $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
+    var requestPOJO = {
+        "className": "Share",
+        "orderMap": {
+            "id": false
+        },
+        "pageMaxSize": ScrollPOJO.pageMaxSize,
+        "currentPageNumber": ScrollPOJO.page || 1,
+        "likeORMap": likeOrMap,
+        "eqMap": {
+            "username": UserPOJO.user.userName,
+            "type": "MATRIX_REPORT_DRAFT",
+            "deleted": false
+        },
+        "inMap": {},
+    };
+    var data = {
+        'queryJson': $.toJSON(requestPOJO)
+    };
+
+    console.log(JSON.stringify(data));
+    $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
 }
 
-var retrieveData_shared_all = function() {
-  var keyword = SearchPOJO.keywords;
-  var likeOrMap = {};
-  if (keyword) {
-    likeOrMap = {
-      "stringalpha": keyword
+var retrieveData_shared_self = function () {
+    var keyword = SearchPOJO.keywords;
+    var likeOrMap = {};
+    if (keyword) {
+        likeOrMap = {
+            "stringalpha": keyword
+        }
     }
-  }
-  var requestPOJO = {
-    "className": "Share",
-    "orderMap": {
-      "id": false
-    },
-    "pageMaxSize": ScrollPOJO.pageMaxSize,
-    "currentPageNumber": ScrollPOJO.page || 1,
-    "likeORMap": likeOrMap,
-    "eqMap": {
-      "type": "MATRIX_REPORT_DRAFT",
-      "tag":"SHARE",
-      "deleted": false
-    },
-    "inMap": {},
-  };
-  var data = {
-    'queryJson': $.toJSON(requestPOJO)
-  };
-  console.log(JSON.stringify(data));
-  $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
+    var requestPOJO = {
+        "className": "Share",
+        "orderMap": {
+            "id": false
+        },
+        "pageMaxSize": ScrollPOJO.pageMaxSize,
+        "currentPageNumber": ScrollPOJO.page || 1,
+        "likeORMap": likeOrMap,
+        "eqMap": {
+            "username": UserPOJO.user.userName,
+            "type": "MATRIX_REPORT_DRAFT",
+            "tag": "SHARE",
+            "deleted": false
+        },
+        "inMap": {},
+    };
+    var data = {
+        'queryJson': $.toJSON(requestPOJO)
+    };
+    console.log(JSON.stringify(data));
+    $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
 }
 
-var retrieveData_template_all = function() {
-  var keyword = SearchPOJO.keywords;
-  var likeOrMap = {
-    "stringbeta":'TEMPLATE'
-  };
-  if (keyword) {
-    likeOrMap = {
-      "stringalpha": keyword,
-      "stringbeta":'TEMPLATE'
+var retrieveData_shared_all = function () {
+    var keyword = SearchPOJO.keywords;
+    var likeOrMap = {};
+    if (keyword) {
+        likeOrMap = {
+            "stringalpha": keyword
+        }
     }
-  }
-  var requestPOJO = {
-    "className": "Share",
-    "orderMap": {
-      "id": false
-    },
-    "pageMaxSize": ScrollPOJO.pageMaxSize,
-    "currentPageNumber": ScrollPOJO.page || 1,
-    "likeORMap": likeOrMap,
-    "eqMap": {
-      "type": "MATRIX_REPORT_DRAFT",
-      "tag":"SHARE",
-      "deleted": false
-    },
-    "inMap": {},
-  };
-  var data = {
-    'queryJson': $.toJSON(requestPOJO)
-  };
-  console.log(JSON.stringify(data));
-  $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
+    var requestPOJO = {
+        "className": "Share",
+        "orderMap": {
+            "id": false
+        },
+        "pageMaxSize": ScrollPOJO.pageMaxSize,
+        "currentPageNumber": ScrollPOJO.page || 1,
+        "likeORMap": likeOrMap,
+        "eqMap": {
+            "type": "MATRIX_REPORT_DRAFT",
+            "tag": "SHARE",
+            "deleted": false
+        },
+        "inMap": {},
+    };
+    var data = {
+        'queryJson': $.toJSON(requestPOJO)
+    };
+    console.log(JSON.stringify(data));
+    $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
+}
+
+var retrieveData_template_all = function () {
+    var keyword = SearchPOJO.keywords;
+    var likeOrMap = {
+        "stringbeta": 'TEMPLATE'
+    };
+    if (keyword) {
+        likeOrMap = {
+            "stringalpha": keyword,
+            "stringbeta": 'TEMPLATE'
+        }
+    }
+    var requestPOJO = {
+        "className": "Share",
+        "orderMap": {
+            "id": false
+        },
+        "pageMaxSize": ScrollPOJO.pageMaxSize,
+        "currentPageNumber": ScrollPOJO.page || 1,
+        "likeORMap": likeOrMap,
+        "eqMap": {
+            "type": "MATRIX_REPORT_DRAFT",
+            "tag": "SHARE",
+            "deleted": false
+        },
+        "inMap": {},
+    };
+    var data = {
+        'queryJson': $.toJSON(requestPOJO)
+    };
+    console.log(JSON.stringify(data));
+    $.serverRequest($.getServerRoot() + '/service_generic_query/api/query', data, "SEARCH_SUCCESS_LISTENER", "PAGING_SEARCH_FAILED", "SERVICE_GENERIC_QUERY_FAILED");
 }
 
 // *******Server Side Retrieve Data Listener JS Code*******
@@ -158,45 +197,45 @@ $.subscribe("PAGING_SEARCH_FAILED", failedListener);
 $.subscribe("SERVICE_GENERIC_QUERY_FAILED", failedServiceListener);
 
 function failedServiceListener() {
-  responseViewModel.errorResponse("SERVICE 'GENERIC QUERY' FAILED! Please contact with the system admin for more information...")
+    responseViewModel.errorResponse("SERVICE 'GENERIC QUERY' FAILED! Please contact with the system admin for more information...")
 }
 
 function failedListener() {
-  if (arguments && arguments[1]) {
-    var errorMessage = arguments[1].errorMessage;
-    responseViewModel.errorResponse(errorMessage)
-  }
+    if (arguments && arguments[1]) {
+        var errorMessage = arguments[1].errorMessage;
+        responseViewModel.errorResponse(errorMessage)
+    }
 }
 
 function successListener() {
-  if (arguments && arguments[1]) {
-    // var headers = AlgorithmPOJO.buildKeys(arguments[1].result);
-    // viewModel.buildSearchData(arguments[1]);
-    console.log(arguments[1]);
-    var data = arguments[1].result;
-    ScrollPOJO.displayResult = ScrollPOJO.displayResult.concat(data);
-    //    console.log(data.length);
-    if (data.length < ScrollPOJO.pageMaxSize) {
-      ScrollPOJO.hasNewData = false;
-    }
-    var results = ScrollPOJO.displayResult.sort(sortTime);
+    if (arguments && arguments[1]) {
+        // var headers = AlgorithmPOJO.buildKeys(arguments[1].result);
+        // viewModel.buildSearchData(arguments[1]);
+        console.log(arguments[1]);
+        var data = arguments[1].result;
+        ScrollPOJO.displayResult = ScrollPOJO.displayResult.concat(data);
+        //    console.log(data.length);
+        if (data.length < ScrollPOJO.pageMaxSize) {
+            ScrollPOJO.hasNewData = false;
+        }
+        var results = ScrollPOJO.displayResult.sort(sortTime);
 
-    viewModel.viewData(results);
-  }
+        viewModel.viewData(results);
+    }
 }
 
 function sortTime(a, b) {
-  return b.createtime - a.createtime;
+    return b.createtime - a.createtime;
 }
 
 function redirect2editor(currentData) {
-  if (currentData && currentData.token) {
-    window.open($.getRootPath() + '/module_report_editor.html?token=' + currentData.token);
-  }
+    if (currentData && currentData.token) {
+        window.open($.getRootPath() + '/module_report_editor.html?token=' + currentData.token);
+    }
 }
 
 function redirect2viewer(currentData) {
-  if (currentData && currentData.token) {
-    window.open($.getRootPath() + '/module_report_viewer.html?token=' + currentData.token);
-  }
+    if (currentData && currentData.token) {
+        window.open($.getRootPath() + '/module_report_viewer.html?token=' + currentData.token);
+    }
 }
